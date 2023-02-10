e640e1cd22a0a504230c6209a021159ae4ab04d8
Айнур Низамов <nizamoff@MacBook-Pro-Ajnur.local>
2023-02-10 13:30:28 +0300

    Methods implementations moved outside the class body

--- stack.h
+++ stack.h
@@ -5,82 +5,31 @@
 template<typename ValueType>
 class Stack {
 public:
-    explicit Stack() : size_(kDefaultSize), capacity_(kDefaultCapacity), array_(new ValueType[kDefaultCapacity]) {}
+    explicit Stack();
 
-    ~Stack() {
-        delete[] array_;
-    }
+    ~Stack();
 
-    Stack(const Stack& other) {
-        Copy(other);
-    }
+    Stack(const Stack& other);
 
-    Stack& operator=(const Stack& other) {
-        if (this == &other) {
-            return *this;
-        }
+    Stack& operator=(const Stack& other);
 
-        Copy(other);
-        return *this;
-    }
+    Stack(Stack&& other) noexcept;
 
-    Stack(Stack&& other) noexcept : size_(other.size_), capacity_(other.capacity_), array_(other.array_) {
-        MakeDefault(*&other);
-    }
+    Stack& operator=(Stack&& other) noexcept;
 
-    Stack& operator=(Stack&& other) noexcept {
-        if (this == &other) {
-            return *this;
-        }
+    ValueType Top() const;
 
-        delete[] array_;
-        size_ = other.size_;
-        capacity_ = other.capacity_;
-        array_ = other.array_;
+    bool Empty() const;
 
-        MakeDefault(*&other);
-        return *this;
-    }
+    size_t Size() const;
 
-    ValueType Top() const {
-        if (size_ == 0) {
-            // return signal 11: SIGSEGV
-            exit(139);
-        }
-        return array_[size_ - 1];
-    }
+    void Clear();
 
-    bool Empty() const {
-        return size_ == 0;
-    }
+    void Push(const ValueType value);
 
-    size_t Size() const {
-        return size_;
-    }
+    void Pop();
 
-    void Clear() {
-        delete[] array_;
-        MakeDefault(*this);
-    }
-
-    void Push(const ValueType value) {
-        if (size_ >= capacity_) {
-            AddMemory();
-        }
-        array_[size_++] = value;
-    }
-
-    void Pop() {
-        if (size_ == 0) {
-            // return signal 11: SIGSEGV
-            exit(139);
-        }
-        --size_;
-    }
-
-    void Swap(Stack& other) {
-        std::swap(*this, other);
-    }
+    void Swap(Stack& other);
 
 private:
     const size_t kDefaultSize = 0;
@@ -90,33 +39,132 @@ private:
     size_t capacity_ = kDefaultCapacity;
     ValueType* array_ = nullptr;
 
-    void AddMemory(size_t new_capacity = 0) {
-        if (new_capacity == 0) {
-            new_capacity = capacity_ * 2;
-        }
+    void AddMemory(size_t new_capacity = 0);
 
-        ValueType* old_array = array_;
-        array_ = new ValueType[new_capacity];
+    void Copy(const Stack& other);
 
-        for (size_t i = 0; i < size_; ++i) {
-            array_[i] = old_array[i];
-        }
-
-        capacity_ = new_capacity;
-        delete[] old_array;
-    }
-
-    void Copy(const Stack& other) {
-        AddMemory(other.capacity_);
-        size_ = other.size_;
-        for (size_t i = 0; i < size_; ++i) {
-            array_[i] = other.array_[i];
-        }
-    }
-
-    void MakeDefault(Stack& other) noexcept {
-        other.size_ = kDefaultSize;
-        other.capacity_ = kDefaultCapacity;
-        other.array_ = nullptr;
-    }
+    void MakeDefault(Stack& other) noexcept;
 };
+
+template<typename ValueType>
+Stack<ValueType>::Stack() : size_(kDefaultSize), capacity_(kDefaultCapacity), array_(new ValueType[kDefaultCapacity]) {}
+
+template<typename ValueType>
+Stack<ValueType>::~Stack() {
+    delete[] array_;
+}
+
+template<typename ValueType>
+Stack<ValueType>::Stack(const Stack &other) {
+    Copy(other);
+}
+
+template<typename ValueType>
+Stack<ValueType> &Stack<ValueType>::operator=(const Stack &other) {
+    if (this == &other) {
+        return *this;
+    }
+
+    Copy(other);
+    return *this;
+}
+
+template<typename ValueType>
+Stack<ValueType>::Stack(Stack &&other) noexcept : size_(other.size_), capacity_(other.capacity_), array_(other.array_) {
+    MakeDefault(*&other);
+}
+
+template<typename ValueType>
+Stack<ValueType> &Stack<ValueType>::operator=(Stack &&other) noexcept {
+    if (this == &other) {
+        return *this;
+    }
+
+    delete[] array_;
+    size_ = other.size_;
+    capacity_ = other.capacity_;
+    array_ = other.array_;
+
+    MakeDefault(*&other);
+    return *this;
+}
+
+template<typename ValueType>
+ValueType Stack<ValueType>::Top() const {
+    if (size_ == 0) {
+        // return signal 11: SIGSEGV
+        exit(139);
+    }
+    return array_[size_ - 1];
+}
+
+template<typename ValueType>
+bool Stack<ValueType>::Empty() const {
+    return size_ == 0;
+}
+
+template<typename ValueType>
+size_t Stack<ValueType>::Size() const {
+    return size_;
+}
+
+template<typename ValueType>
+void Stack<ValueType>::Clear() {
+    delete[] array_;
+    MakeDefault(*this);
+}
+
+template<typename ValueType>
+void Stack<ValueType>::Push(const ValueType value) {
+    if (size_ >= capacity_) {
+        AddMemory();
+    }
+    array_[size_++] = value;
+}
+
+template<typename ValueType>
+void Stack<ValueType>::Pop() {
+    if (size_ == 0) {
+        // return signal 11: SIGSEGV
+        exit(139);
+    }
+    --size_;
+}
+
+template<typename ValueType>
+void Stack<ValueType>::Swap(Stack &other) {
+    std::swap(*this, other);
+}
+
+template<typename ValueType>
+void Stack<ValueType>::AddMemory(size_t new_capacity) {
+    if (new_capacity == 0) {
+        new_capacity = capacity_ * 2;
+    }
+
+    ValueType *old_array = array_;
+    array_ = new ValueType[new_capacity];
+
+    for (size_t i = 0; i < size_; ++i) {
+        array_[i] = old_array[i];
+    }
+
+    capacity_ = new_capacity;
+    delete[] old_array;
+}
+
+template<typename ValueType>
+void Stack<ValueType>::Copy(const Stack &other) {
+    AddMemory(other.capacity_);
+    size_ = other.size_;
+    for (size_t i = 0; i < size_; ++i) {
+        array_[i] = other.array_[i];
+    }
+}
+
+template<typename ValueType>
+void Stack<ValueType>::MakeDefault(Stack &other) noexcept {
+    other.size_ = kDefaultSize;
+    other.capacity_ = kDefaultCapacity;
+    other.array_ = nullptr;
+}
